35aacec0875f9d8c8d03481201f17b6381f735c9
16385703 <lucianapaolagomez@gmail.com>
2020-07-28 14:26:09 -0300

    Add arrays practice

--- learningjs.js
+++ learningjs.js
@@ -1,3 +1,17 @@
-let changeMe= true
-changeMe = false
-console.log(changeMe)
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+const listItem = famousSayings[0];
+console.log(listItem);
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0]= 'Mayo';
+console.log(condiments);
+
+condiments =['Mayo'];
+console.log(condiments);
+
+utensils[3]= 'Spoon';
+console.log(utensils);
+//utensils = ['Spoon']; utensils is not mutable because it is declared using const
