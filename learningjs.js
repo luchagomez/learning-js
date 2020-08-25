2f4eac3841bb217ecc64a58a914d5351c7437c60
16385703 <lucianapaolagomez@gmail.com>
2020-08-25 19:33:39 -0300

    some js learnings

--- learningjs.js
+++ learningjs.js
@@ -6,12 +6,26 @@ let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 
 const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
 
-condiments[0]= 'Mayo';
-console.log(condiments);
+//condiments[0]= 'Mayo';
+//console.log(condiments);
 
-condiments =['Mayo'];
-console.log(condiments);
+//condiments =['Mayo'];
+//console.log(condiments);
 
-utensils[3]= 'Spoon';
+//utensils[3]= 'Spoon';
 console.log(utensils);
-//utensils = ['Spoon']; utensils is not mutable because it is declared using const
+utensils = ['Spoon']; //utensils is not mutable because it is declared using const
+
+//const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+//const orangeJuice= groceryList.shift();
+//console.log(groceryList);
+//console.log(orangeJuice);
+
+function addUtensil (arr){
+    arr.push('Spoon');
+}
+
+addUtensil(utensils);
+
+console.log(utensils);
